05072c62fb16330f2e61bf85c46f687346d467ef
Lauren <llyne@my.hpu.edu>
2019-09-13 20:45:15 -1000

    Commit for second hw02 program

---
+++ hw02B/function.cc
@@ -0,0 +1,10 @@
+#include "function.h"
+
+void swap(int *a, int *b)
+{
+    int t;
+    t = *a;
+    *a=*b;
+    *b=t;
+    
+    }
---
+++ hw02B/main.cc
@@ -0,0 +1,11 @@
+#include <cstdio>
+
+int main(int argc, char** argv)
+{
+    int stdin;
+    int stdout;
+   
+    swap(&stdin,&stdout);
+    printf("stdin=%d stdout=%d\n",stdin,stdout);
+
+}
