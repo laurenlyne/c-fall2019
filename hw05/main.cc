8da03188bd1a467269df4dcdd81a4bfcdb344c9d
laurenlyne <35615234+laurenlyne@users.noreply.github.com>
2019-10-12 05:32:24 -1000

    Add files via upload

--- hw05/main.cc
+++ hw05/main.cc
@@ -2,12 +2,11 @@
 #include "function.h"
 
 int main() {
-    char fileName[50]= "test.ppm";
+    const char* fileName = "test.ppm";
     int width =0;
     int height=0;
     int size=width*height;
     int max =255;
-    char pSix[5]="P6";
     unsigned char* pixel= readPPM(fileName, &width, &height, &max);
  
 
--- hw05/read.cc
+++ hw05/read.cc
@@ -1,17 +1,17 @@
 #include<stdio.h>
 #include "function.h"
 
-unsigned char* readPPM(const char* fileName,int* width, int* height, int* max)
+unsigned char* readPPM(FILE* fileName,int* width, int* height, int* max)
 {
-    FILE* fp = fopen("ppm.txt","rb");
+    fileName= fopen("ppm.txt","rb");
     
     char pSix[3];
-    fscanf(fp,"%s\n", pSix);
-    fscanf(fp,"%d\n %d\n",width,height);
-    fscanf(fp,"%d\n", max);
+    fscanf(fileName,"%s\n", pSix);
+    fscanf(fileName,"%d\n %d\n",width,height);
+    fscanf(fileName,"%d\n", max);
     
     unsigned char* pixel=  new unsigned char[3 * *width * *height];
-    fread(pixel, sizeof(pixel),3 * *width * *height,fp);
-    fclose(fp);
+    fread(pixel, sizeof(pixel),3 * *width * *height,fileName);
+    fclose(fileName);
     return pixel;
     }
